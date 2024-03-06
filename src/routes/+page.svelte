6a5c5be94ff75503a5daff9444e8618eb303895f
Fyxren <benweerd@gmail.com>
2024-03-06 21:57:49 +0100

    feat: add meta tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,29 @@
 
 </script>
 
+<svelte:head>
+    <title>Fyxren Inc.</title>
+    <meta name="description" content="The place to be for all your needs. Only the best services and products for you." />
+    <meta name="keywords" content="Fyxren, Fyxren Inc., Fyxren Inc. 2024, Fyxren 2024, Fyxren Inc. All rights reserved." />
+    <meta name="author" content="Fyxren Inc." />
+    <meta name="robots" content="noindex, nofollow" />
+    <meta name="theme-color" content="#112" />
+
+    <meta property="og:title" content="Fyxren Inc." />
+    <meta property="og:description" content="The place to be for all your needs. Only the best services and products for you." />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://fyxren.com" />
+    <meta property="og:image" content="https://github.com/Fyxren.png" />
+    <meta property="og:site_name" content="Fyxren Inc." />
+    <meta property="og:locale" content="en_US" />
+    
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@Fyxren" />
+    <meta name="twitter:creator" content="@Fyxren" />
+    <meta name="twitter:title" content="Fyxren Inc." />
+
+</svelte:head>
+
 <div class="h-full">
     <Banner />
     <Hero />
